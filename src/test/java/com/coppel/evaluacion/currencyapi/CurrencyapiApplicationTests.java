2a4e704847a6423aa4c32d5f1c2abf4d6b054779
Miguel Lara <miguel.laraobregon@gmail.com>
2024-11-05 17:52:08 -0600

    Commit inicial, creamos proyecto spring boot

---
+++ src/test/java/com/coppel/evaluacion/currencyapi/CurrencyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coppel.evaluacion.currencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
